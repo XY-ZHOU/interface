86695a0011e93f9047a96d91554195ea6e0b680b
XY-ZHOU <731986251@qq.com>
2019-02-19 13:53:51 +0800

    how many plans to choose

--- ChooseTransportation/src/main/java/service/Choose.java
+++ ChooseTransportation/src/main/java/service/Choose.java
@@ -8,6 +8,24 @@ import main.java.model.Transportation;
 import java.util.ArrayList;
 
 public class Choose {
+    public static void main(String[] args) {
+        Transportation selfDriving = new SelfDriving(500);
+        Transportation bus = new Bus(300);
+        Transportation train = new Train(400);
+        Transportation plane = new SelfDriving(1000);
 
+        int num = 0;
+        ArrayList<Integer> priceArr = new ArrayList<Integer>();
+        priceArr.add(selfDriving.getPrice());
+        priceArr.add(bus.getPrice());
+        priceArr.add(train.getPrice());
+        priceArr.add(plane.getPrice());
+        for (int i = 0; i < priceArr.size(); i++) {
+            if (500 >= priceArr.get(i)) {
+                num++;
+            }
+        }
+        System.out.println("有" + num + "方案可以选择");
+    }
 }
 
