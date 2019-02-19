a85dbb8daf2c9643f96f9e777febfa90600813c2
XY-ZHOU <731986251@qq.com>
2019-02-19 13:42:46 +0800

    class of chicken

--- BuyChicken/src/main/java/model/Chicken.java
+++ BuyChicken/src/main/java/model/Chicken.java
@@ -1,6 +1,13 @@
 package main.java.model;
 
 public class Chicken {
+    private double price;
 
-    
+    public Chicken(double price) {
+        this.price = price;
+    }
+
+    public int getMaxCount(int money) {
+        return (int) (money / price);
+    }
 }
