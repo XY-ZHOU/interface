79b7d033b028a4c3d4dd2877f2a2787b4c41de34
XY-ZHOU <731986251@qq.com>
2019-02-19 13:38:40 +0800

    init project

---
+++ BuyChicken/src/main/java/model/Chicken.java
@@ -0,0 +1,6 @@
+package main.java.model;
+
+public class Chicken {
+
+    
+}
---
+++ ChooseTransportation/src/main/java/service/Choose.java
@@ -0,0 +1,13 @@
+package main.java.service;
+
+import main.java.model.Bus;
+import main.java.model.SelfDriving;
+import main.java.model.Train;
+import main.java.model.Transportation;
+
+import java.util.ArrayList;
+
+public class Choose {
+
+}
+
