723e8298fa21665f1d967f2f003470f8dd0c7901
XY-ZHOU <731986251@qq.com>
2019-02-21 21:27:45 +0800

    rewrite the file of Context

--- ChooseTransportation/src/main/java/service/Context.java
+++ ChooseTransportation/src/main/java/service/Context.java
@@ -5,6 +5,19 @@ import main.java.model.*;
 import java.util.ArrayList;
 
 public class Context {
-    
+    public static ArrayList<Integer> allTranspots = new ArrayList<>();
+
+    static {
+        SelfDriving selfDriving = new SelfDriving();
+        Bus bus = new Bus();
+        Train train = new Train();
+        Plane plane = new Plane();
+        allTranspots.add(selfDriving.getPrice());
+        allTranspots.add(bus.getPrice());
+        allTranspots.add(train.getPrice());
+        allTranspots.add(plane.getPrice());
+    }
+
+
 }
 
