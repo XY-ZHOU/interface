6e6c436e7d30246d1f3bada1fc98337605f81b57
XY-ZHOU <731986251@qq.com>
2019-02-21 21:26:36 +0800

    rename

--- ChooseTransportation/src/main/java/service/Context.java
+++ ChooseTransportation/src/main/java/service/Context.java
@@ -1,31 +1,10 @@
 package main.java.service;
 
-import main.java.model.Bus;
-import main.java.model.SelfDriving;
-import main.java.model.Train;
-import main.java.model.Transportation;
+import main.java.model.*;
 
 import java.util.ArrayList;
 
-public class Choose {
-    public static void main(String[] args) {
-        Transportation selfDriving = new SelfDriving(500);
-                    Transportation bus = new Bus(300);
-        Transportation train = new Train(400);
-        Transportation plane = new SelfDriving(1000);
-
-        int num = 0;
-        ArrayList<Integer> priceArr = new ArrayList<Integer>();
-        priceArr.add(selfDriving.getPrice());
-        priceArr.add(bus.getPrice());
-        priceArr.add(train.getPrice());
-        priceArr.add(plane.getPrice());
-        for (int i = 0; i < priceArr.size(); i++) {
-            if (500 >= priceArr.get(i)) {
-                num++;
-            }
-        }
-        System.out.println("有" + num + "方案可以选择");
-    }
+public class Context {
+    
 }
 
